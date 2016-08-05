a10c347f4f4c7d38b8e6d767f4befc3c0b39aa31
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-08-05 20:10:18 +0530

    bugs(search & results): Fix all the fixmes

--- scripts/results.js
+++ scripts/results.js
@@ -4,43 +4,24 @@
 
   let app = document.getElementById('app');
   HotelDemo.mixinSettings(app);
-  let baseApiEndpoint = HotelDemo.baseApiEndpoint;
-  let genUrl = HotelDemo.genUrl.bind(null, baseApiEndpoint);
-  let criteria = HotelDemo.getItem('criteria');
 
+  let criteria = HotelDemo.getItem('criteria');
+  let selections = [
+    'Name', 'Rating', 'Address', 'HeroImageUrl', 'StayDuration',
+    'IsPostPaid', 'Source', 'Id', 'Deal', 'Fare'
+  ];
+
+  app.token = HotelDemo.getItem('token');
+  app.sid = HotelDemo.getItem('sid');
+  app.selections = selections.join(',');
+  app.apiBaseUrl = HotelDemo.baseApiEndpoint;
   app.loading = false;
   HotelDemo.mixinSearchRecap(app);
 
   app.addEventListener('dom-change', function() {
-    let token = HotelDemo.getItem('token');
-    let sid = HotelDemo.getItem('sid');
     let results = app.$.results;
-    let selections = [
-      'Name', 'Rating', 'Address', 'HeroImageUrl', 'StayDuration',
-      'IsPostPaid', 'Source', 'Id', 'Deal', 'Fare'
-    ];
 
-    // FIXME: there should be a component that takes base endpoint,
-    // api endpoint and query params object to generate a complete url
-    // or there should be a mystique specific url generator that takes
-    // product name and end point type to generate the url. This fix
-    // might apply at all the places where `genUrl` is used.
-    let apiEndpoint = `api/hotel/results/${sid}/rates`;
-    results.dataApi = genUrl(apiEndpoint, {
-      "$top": 20, token: token, "$select": selections.join(',')
-    });
-
-    apiEndpoint = `api/hotel/filter/${sid}/rates`;
-    results.filterApi = genUrl(apiEndpoint, {
-      token: token
-    });
-
-    apiEndpoint = `api/hotel/results/filter/${sid}/rates`;
-    results.filterResultsApi = genUrl(apiEndpoint, {
-      "$top": 20, token: token, "$select": selections.join(',')
-    });
-
-    // TODO: are this parameters common to any hotel results call (mystique)
+    // TODO: are this ,parameters common to any hotel results call (mystique)
     results.sortParams = [
       { "key": "Name", "value": "$orderby=Name" },
       { "key": "Rating", "value": "$orderby=Rating" , "default": true },
@@ -52,23 +33,15 @@
 
     results.generateList();
 
-    // FIXME: you shouldn't have to run `generateFilter` this way
-    // you should be able to pass and configure your own set of filters
     results.addEventListener('list-population', function(ev) {
-      let filter = Polymer.dom(results.root);
-      filter.querySelector('t-filter').generateFilter();
+      results.generateFilter();
     });
 
     let singleItineraryCall = app.$.singleItineraryCall;
 
     results.addEventListener('item-selected', function(ev) {
       app.loading = true;
-
-      let apiEndpoint = `api/hotel/results/${sid}/rates`;
-      singleItineraryCall.url = genUrl(apiEndpoint, {
-        token: token, "$filter": `Id eq '${event.detail.id}'`
-      });
-
+      app.selectedHotelId = event.detail.id;
       singleItineraryCall.generateRequest();
     });
   });
--- scripts/search.js
+++ scripts/search.js
@@ -6,73 +6,33 @@
   HotelDemo.mixinSettings(app);
   let baseApiEndpoint = HotelDemo.baseApiEndpoint;
 
+  app.apiBaseUrl = baseApiEndpoint;
   app.authApiEndpoint = `${baseApiEndpoint}api/Authentication/Authenticate/context`;
 
   app.addEventListener('dom-change', function() {
-    let mysAuth = app.$.mysAuth;
     let search = app.$.searchComponent;
-    let provider = app.$.searchProvider;
     let toast = app.$.toast;
 
-    app.apiBaseUrl = baseApiEndpoint;
-    app.apiSearchUrl = 'api/hotel/search';
-
-    // FIXME: t-mystique-auth should extract the token from response
-    mysAuth.addEventListener('token-response-changed', function(ev) {
-      let token;
-
-      try {
-        token = ev.detail.value.authenticationToken
-      } catch (e) {
-        toast.text = e.toString();
-        toast.open();
-      }
-
-      HotelDemo.setItem('token', token);
-      app.authToken = token;
-
-      // FIXME: `location` is the autosuggest component
-      // hotel search should expose api for this
-      search.$.location.dataUrl = `${baseApiEndpoint}api/content/autosuggest/`;
-      search.$.location.queryParams = `token=${token}`;
+    search.addEventListener('t-hotel-search-validate', function(ev) {
+      app.searchedData = ev.detail;
+      search.showProgress(true);
     });
-
-    // FIXME: the event name `t-hotel-search` doesn't make any sense
-    // TODO: should you have to do this wiring manually?
-    search.addEventListener('t-hotel-search', function(ev) {
-      provider.search(ev.detail);
-
-      // FIXME: why was this removed from code in `t-hotel-search`
-      // look at the commit `52ee94d1`
-      search.$.search.label = 'SEARCHING HOTELS...';
-      search.$.search.disabled = true;
-    });
-
-    provider.addEventListener('request', function(ev) {
-      if (!ev.detail.body) {
-        return;
-      }
-
-      HotelDemo.setItem('criteria', JSON.parse(ev.detail.body));
-    });
-
-    // FIXME: the provider expects a function by following name
-    // it should fire an event which can be set up declaratively through markup
-    provider._successHandler = function(ev) {
-      try {
-        HotelDemo.setItem('sid', ev.detail.response.searchId);
-        HotelDemo.redirectToResults();
-      } catch (e) {
-        toast.text = 'Bad response from search api';
-        toast.open();
-      }
-    };
-
-    provider._errorHandler = function(ev) {
-      search.$.search.label = 'SEARCH HOTELS';
-      search.$.search.disabled = false;
-
-      HotelDemo.errorHandler.call(app, ev);
-    };
   });
+
+  app._successHandler = function(ev) {
+    try {
+      HotelDemo.setItem('sid', ev.detail.searchId);
+      HotelDemo.setItem('token', app.tokenResponse.authenticationToken);
+      HotelDemo.setItem('criteria', app.searchRequest);
+      HotelDemo.redirectToResults();
+    } catch (e) {
+      toast.text = 'Bad response from search api';
+      toast.open();
+    }
+  };
+
+  app._errorHandler = function(ev) {
+    search.showProgress(true);
+    HotelDemo.errorHandler.call(app, ev);
+  };
 })(window, document);

c16e449a19a2ecfff1ecda082828b36d0d310850
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2016-08-09 14:13:37 +0530

    Deploy

--- scripts/mixin-settings.js
+++ scripts/mixin-settings.js
@@ -2,6 +2,25 @@
   window.HotelDemo = window.HotelDemo || {};
   let HotelDemo = window.HotelDemo;
 
+  function isString(val) {
+    return Object.prototype.toString.call(val) === '[object String]';
+  }
+
+  function getThemeUrl(themeOptions) {
+    let qs = themeOptions
+      .map(opt => {
+        let value = encodeURIComponent(opt.value + (opt.unit || ''));
+        let key = encodeURIComponent(opt.name);
+
+        return `${key}=${value}`;
+      });
+
+    qs = qs.join('&');
+
+    let proto = window.location.protocol;
+    return `${proto}://theme-atomproject.rhcloud.com/theme.html?` + qs;
+  }
+
   HotelDemo.mixinSettings = function(app) {
     app.redirectToCustomize = function() {
       HotelDemo.redirectToCustomize();
@@ -15,49 +34,40 @@
     app.theme = HotelDemo.getItem('theme') || 0;
     app.themeOptions = HotelDemo.getItem('theme-options');
 
-    let _themes = [
-      'components/theme-default.html',
-      'components/theme-red.html',
-      'components/theme-purple.html'
-    ];
-
-    app.themeUrl = function(idx) {
-      return _themes[idx];
-    };
-
-    app.getThemeUrl = (function() {
-      let _count = 0;
-
-      return function(themeOptions) {
-        let qs = themeOptions
-          .map(opt => {
-            let value = encodeURIComponent(opt.value + (opt.unit || ''));
-            let key = encodeURIComponent(opt.name);
-
-            return `${key}=${value}`;
-          });
-        
-        qs.push(`v=${_count++}`);
-        qs = qs.join('&');
-
-        return 'http://theme-atomproject.rhcloud.com/theme.html?' + qs;
-      };
-    })();
+    app.themes = [{
+      label: 'Default',
+      url: 'components/theme-default.html'
+    }, {
+      label: 'Red',
+      url: 'components/theme-red.html'
+    }, {
+      label: 'Purple',
+      url: 'components/theme-purple.html'
+    }, {
+      label: 'Custom',
+      url: function() { return getThemeUrl(app.themeOptions); }
+    }];
 
     app.switchTheme = (function() {
       let _count = 0;
 
+      function appendCount(url) {
+        let c = url.indexOf('?') === -1 ? '?' : '&';
+        return `${url}${c}v=${_count++}`;
+      }
+
       return function(idx) {
-        let themes = _themes.map(theme => `${theme}?v=${_count}`);
-        _count++;
-        Polymer.Base.importHref(themes[idx]);
+        if (idx < 0 || idx > app.themes.length) {
+          return;
+        }
+
+        let themeUrl = app.themes[idx].url;
+        themeUrl = !isString(themeUrl) ? themeUrl() : themeUrl;
+
+        Polymer.Base.importHref(appendCount(themeUrl));
       };
     })();
 
-    if (!app.themeOptions) {
-      app.switchTheme(app.theme);
-    } else {
-      Polymer.Base.importHref(app.getThemeUrl(app.themeOptions));
-    }
+    app.switchTheme(app.theme);
   };
 })(window, document);

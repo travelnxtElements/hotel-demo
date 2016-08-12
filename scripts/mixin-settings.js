84ea305a5504933824d15891e9bdc8ab06836af4
Ganesh Ratnaparkhi <ganeshr43@gmail.com>
2016-08-12 15:27:05 +0530

    Bug(Demo):Demo feedback changes

--- scripts/mixin-settings.js
+++ scripts/mixin-settings.js
@@ -27,22 +27,29 @@
     };
 
     app.logoUrl = HotelDemo.getItem('logo-url') || 'https://s3.amazonaws.com/tavisca.samplebucket/124850/Images/c9154425-43be-4a7a-9d44-463c97a58040';
-    app.supportEmail = HotelDemo.getItem('support-email') || 'swat@tavisca.com';
-    app.supportPhone = HotelDemo.getItem('support-phone') || '800-459-1597';
-    app.tosUrl = HotelDemo.getItem('tos-url') || 'http://www.bluegreenvacations.com/terms-of-use';
-    app.policyUrl = HotelDemo.getItem('policy-url') || 'http://www.bluegreenvacations.com/privacy-policy';
+    app.supportEmail = HotelDemo.getItem('support-email') || 'sales@tavisca.com';
+    app.supportPhone = HotelDemo.getItem('support-phone') || '0912067083105';
+    app.tosUrl = HotelDemo.getItem('tos-url') || 'http://demo.travelnxt.com/terms';
+    app.policyUrl = HotelDemo.getItem('policy-url') || 'http://demo.travelnxt.com/privacypolicy';
     app.theme = HotelDemo.getItem('theme') || 0;
     app.themeOptions = HotelDemo.getItem('theme-options');
 
     app.themes = [{
-      label: 'Default',
+      label: 'TravelNxt theme',
       url: 'components/theme-default.html'
+    // }, {
+    //   label: 'Red',
+    //   url: 'components/theme-red.html'
+    // }, {
+    //   label: 'Purple',
+    //   url: 'components/theme-purple.html'
+    // }, {
     }, {
-      label: 'Red',
-      url: 'components/theme-red.html'
+      label: 'Apply Hotels.com theme',
+      url: 'components/theme-hotels-com.html'
     }, {
-      label: 'Purple',
-      url: 'components/theme-purple.html'
+      label: 'Apply Expedia theme',
+      url: 'components/theme-expedia.html'
     }, {
       label: 'Custom',
       url: function() { return getThemeUrl(app.themeOptions); }
